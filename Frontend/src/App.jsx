1bd56f18bcfdfdfab85818c5eac1bfc51e32925c
Anshul <anshulchanpuria18@gmail.com>
2024-08-02 15:16:44 +0530

    new Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,8 @@ import SignUpPage from "./pages/SignUpPage";
 import JobListingPage from "./pages/JobListingPage";
 import PostJobPage from "./pages/PostJobPage";
 import JobCategoryPage from "./pages/JobCategoryPage";
+import CompanyListing from "./pages/CompanyListing";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -17,6 +19,9 @@ function App() {
         <Route path="/joblisting" element={<JobListingPage />} />
         <Route path="/postjob" element={<PostJobPage />} />
         <Route path="/jobcategory" element={<JobCategoryPage />} />
+        <Route path="/companylisting" element={<CompanyListing />} />
+        <Route path="/candidate-dashboard" element={<Dashboard />} />
+
       </Routes>
     </>
   );
---
+++ Frontend/src/pages/CompanyListing.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../component/Header";
+import Filter from "../component/Filter";
+import Footer from "../component/Footer";
+import PostAJob from "../component/PostAJob";
+function CompanyListing() {
+  return (
+    <div>
+      <Header></Header>
+      <PostAJob />
+
+      <div className="px-16 py-8">
+        <Filter />
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default CompanyListing;

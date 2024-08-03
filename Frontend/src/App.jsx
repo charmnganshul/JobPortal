9bc4eabe81bdae25b7226df8a49195c7b25de879
deeepikaaa91 <deeepikaaa91@gmail.com>
2024-08-03 21:11:57 +0530

    JobDetail

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,8 @@
  import { Routes,Route } from 'react-router-dom'
 import Hero from './Component/Hero'
 import Login from './Component/Login'
+import JobLishting from './Component/JobLishting'
+import JobDetail from './Component/JobDetail'
 
  
 function App() {
@@ -10,9 +12,9 @@ return (
     <>
      <Routes>
       <Route path='/hero' element={<Hero/>}></Route>
-      <Route path='/login' element={<Login/>}>
-      
-      </Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/joblishting' element={<JobLishting/>}></Route>
+      <Route path='/jobDetail' element={<JobDetail/>}></Route>
      </Routes>    
     </>
   )
